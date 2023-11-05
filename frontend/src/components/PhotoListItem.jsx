a8f405c5d2dd160c9986f622b72a9ea7185fc6c6
linkwithlionhart <linkwithlionhart@gmail.com>
2023-11-05 09:33:41 -0800

    build to display a photo

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -14,8 +14,25 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+
+  const { id, location, imageSource, username, profile } = props.sampleDataForPhotoListItem;
+
+  return (
+    <article className="photo-list__item" key={id}>
+      <img src={imageSource} className="photo-list__image"></img>
+      <div className="photo-list__user-details">
+        <img src={profile} className="photo-list__user-profile"></img>
+        <div className="photo-list__user-info">
+          <span>{username}</span>
+          <span className="photo-list__user-location">
+            {location.city}, {location.country}
+          </span>
+        </div>
+      </div>
+    </article>
+  );
+  
 };
 
 export default PhotoListItem;
