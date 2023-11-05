0cfe95726a7889f851b35fa4435d5bd184d8945a
linkwithlionhart <linkwithlionhart@gmail.com>
2023-11-05 10:41:12 -0800

    refactor with mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,29 +1,12 @@
 import React from "react";
 import TopicListItem from "./TopicListItem";
+import topics from "../mocks/topics"; // TODO: Consume in App.jsx
 
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
 const TopicList = () => {
   
-  const topicList = sampleDataForTopicList.map((topic) => {
+  const topicList = topics.map((topic) => {
     return (<TopicListItem topicTitle={topic.title} key={topic.id} />);
   });
 
