43fa1e73a652a149be3f89cba3c32b99fc6d7bff
linkwithlionhart <linkwithlionhart@gmail.com>
2023-11-05 10:35:13 -0800

    create the home page layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import PhotoList from "./components/PhotoList";
-import TopNavigationBar from "./components/TopNavigationBar";
+import HomeRoute from "routes/HomeRoute";
 
 import './App.scss';
 
@@ -8,8 +7,7 @@ const App = () => {
     
   return (
     <div className="App">
-      <TopNavigationBar />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
