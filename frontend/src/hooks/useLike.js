9a199156a13c4a541b007e390d601dd423ea1d5e
linkwithlionhart <linkwithlionhart@gmail.com>
2023-11-06 12:33:54 -0800

    Implement useReducer to custom hooks

--- frontend/src/hooks/useLike.js
+++ frontend/src/hooks/useLike.js
@@ -1,13 +1,43 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useReducer } from "react";
+
+const actionTypes = {
+  ADD_FAVORITE: "ADD_FAVORITE",
+  REMOVE_FAVORITE: "REMOVE_FAVORITE",
+};
+
+const likeReducer = (state, action) => {
+  let newState;
+  switch (action.type) {
+  case actionTypes.ADD_FAVORITE:
+    return {
+      ...state,
+      [action.photoId]: true,
+    };
+  case actionTypes.REMOVE_FAVORITE:
+    newState = { ...state };
+    delete newState[action.photoId];
+    return newState;
+  default:
+    return state;
+  }
+};
 
 const useLike = () => {
-  const [ like, setLike ] = useState({});
+
+  const [like, dispatch] = useReducer(likeReducer, {});
+  // const [ like, setLike ] = useState({});
 
   const toggleLike = (photoId) => {
-    setLike((prevLikes) => ({
-      ...prevLikes,
-      [photoId] : !prevLikes[photoId] || false,
-    }));
+    // setLike((prevLikes) => ({
+    //   ...prevLikes,
+    //   [photoId] : !prevLikes[photoId] || false,
+    // }));
+    if (like[photoId]) {
+      dispatch({ type: actionTypes.REMOVE_FAVORITE, photoId });
+    } else {
+      dispatch({ type: actionTypes.ADD_FAVORITE, photoId });
+    }
   };
 
   return {
--- frontend/src/hooks/useModal.js
+++ frontend/src/hooks/useModal.js
@@ -1,23 +1,62 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useReducer } from "react";
+
+const ACTIONS = {
+  OPEN_MODAL: "OPEN_MODAL",
+  CLOSE_MODAL: "CLOSE_MODAL",
+};
+
+const modalReducer = (state, action) => {
+  let similarPhotoObjs;
+  switch (action.type) {
+  case ACTIONS.OPEN_MODAL:
+    similarPhotoObjs = Object.values(action.similarPhotos);
+    return {
+      clicked: true,
+      modalPhotos: { photo: action.photo, similarPhotos: similarPhotoObjs },
+    };
+  case ACTIONS.CLOSE_MODAL:
+    return {
+      clicked: false,
+      modalPhotos: {},
+    };
+  default:
+    return state;
+  }
+};
 
 const useModal = () => {
-  
-  const [ clicked, setClicked ] = useState(false);
-  const [ modalPhotos, setModalPhotos ] = useState({});
+
+  const initialState = {
+    clicked: false,
+    modalPhotos: {},
+  };
+
+  const [state, dispatch] = useReducer(modalReducer, initialState);
+  // const [ clicked, setClicked ] = useState(false);
+  // const [ modalPhotos, setModalPhotos ] = useState({});
 
   const isClicked = (photo, similarPhotos) => {
-    setClicked(true);
-    const similarPhotoObjs = Object.values(similarPhotos);
-    setModalPhotos({ photo, similarPhotos: similarPhotoObjs });
+    // setClicked(true);
+    // const similarPhotoObjs = Object.values(similarPhotos);
+    // setModalPhotos({ photo, similarPhotos: similarPhotoObjs });
+    dispatch({
+      type: ACTIONS.OPEN_MODAL,
+      photo,
+      similarPhotos,
+    });
   };
 
   const unClicked = () => {
-    setClicked(false);
+    // setClicked(false);
+    dispatch({
+      type: ACTIONS.CLOSE_MODAL,
+    });
   };
 
   return {
-    clicked,
-    modalPhotos,
+    clicked: state.clicked,
+    modalPhotos: state.modalPhotos,
     isClicked,
     unClicked
   };
