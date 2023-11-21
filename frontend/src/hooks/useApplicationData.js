bdd9975fdabe7e98740504a0016bebe498de1e5a
linkwithlionhart <linkwithlionhart@gmail.com>
2023-11-20 22:22:07 -0800

    Refactor hook: useApplicationData

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,23 +1,29 @@
 import { useState, useEffect } from 'react';
 
 const useApplicationData = () => {
-  
-  const [ photos, setPhotos ] = useState([]);
-  const [ topics, setTopics ] = useState([]);
+  const [photos, setPhotos] = useState([]);
+  const [topics, setTopics] = useState([]);
+
+  const fetchData = async(url, setState) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setState(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    fetch("/api/photos")
-      .then((res) => res.json().then((data) => setPhotos([...data])));
+    fetchData("/api/photos", setPhotos);
   }, []);
 
   useEffect(() => {
-    fetch("/api/topics")
-      .then((res) => res.json().then((data) => setTopics([...data])));
+    fetchData("/api/topics", setTopics);
   }, []);
 
-  const getPhotosByTopics = topicId => {
-    fetch(`/api/topics/photos/${topicId}`)
-      .then((res) => res.json().then((data) => setPhotos([...data])));
+  const getPhotosByTopics = async(topicId) => {
+    await fetchData(`/api/topics/photos/${topicId}`, setPhotos);
   };
 
   return {
@@ -25,7 +31,6 @@ const useApplicationData = () => {
     topics,
     getPhotosByTopics
   };
-
 };
 
 export default useApplicationData;
